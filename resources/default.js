fca8aa9e06d5f9e6883f22a291ae18040649ad4b
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-01-07 13:37:01 +0800

    Create default.js

---
+++ resources/default.js
@@ -0,0 +1,9 @@
+      // If `prefers-color-scheme` is not supported, fall back to light mode.
+      // In this case, light.css will be downloaded with `highest` priority.
+      if (window.matchMedia('(prefers-color-scheme)').media === 'not all') {
+        document.documentElement.style.display = 'none';
+        document.head.insertAdjacentHTML(
+          'beforeend',
+          '<link rel="stylesheet" href="https://cdn.jsdelivr.net/gh/thedoggybrad/speedtest@main/resources/light.css" onload="document.documentElement.style.display = ``">',
+        );
+      }
