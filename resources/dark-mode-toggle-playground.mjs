e2c111f42de8b955c9dbd60868fb7cdcbb903710
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-13 15:10:02 +0800

    Update dark-mode-toggle-playground.mjs

--- resources/dark-mode-toggle-playground.mjs
+++ resources/dark-mode-toggle-playground.mjs
@@ -25,7 +25,7 @@
     // so we need the `color` value.
     themeColor.content = getComputedStyle(body).color;
     colorScheme.content = e.detail.colorScheme;
-    icon.href = e.detail.colorScheme === 'dark' ? 'favicon.png' : 'favicon.png';
+    icon.href = e.detail.colorScheme === 'dark' ? 'resources/favicon.png' : 'resources/favicon.png';
     console.log(
         `${e.target.id} changed the color scheme to ${e.detail.colorScheme}`,
     );
