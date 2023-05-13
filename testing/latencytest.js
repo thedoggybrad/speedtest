e995bf27ee0751c4288704261f066e983a0621c3
TheDoggyBrad <94173621+thedoggybrad@users.noreply.github.com>
2023-05-13 14:32:02 +0800

    Delete testing directory

--- testing/latencytest.js
+++
@@ -1,5 +0,0 @@
-var start = new Date().getTime();
-$('#junkOne').attr('src', 'https://cloudflare.com').on('error', function () {
-    var end = new Date().getTime();
-    $('#timer').html("" + (end-start) + " ms");
-});
