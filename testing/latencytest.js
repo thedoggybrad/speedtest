2cf710f6946cc11e135732364378d42585efa49c
TheDoggyBrad Software Lab <94173621+thedoggybrad@users.noreply.github.com>
2023-08-04 11:30:51 +0800

    Update latencytest.js

--- testing/latencytest.js
+++ testing/latencytest.js
@@ -1,5 +1,5 @@
 var start = new Date().getTime();
 $('#latency-injection').attr('src', 'https://google.com').on('error', function () {
     var end = new Date().getTime();
-    $('#timer').html("" + (end-start) + " ms");
+    $('#latency-result').html("" + (end-start) + " ms");
 });
